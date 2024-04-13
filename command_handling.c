2cef393801e18d68c762e293b5cef1194f77e078
Dominik Kaiser <dkaiser@2-E-4.42heilbronn.de>
2024-04-13 14:45:30 +0200

    Add command list printing

--- command_handling.c
+++ command_handling.c
@@ -6,10 +6,11 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 14:38:47 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/04/13 14:39:09 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/04/13 14:43:45 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
 #include "push_swap.h"
 
 static int	add_cmd_to_queue(t_list **cmds, enum e_pscmd cmd)
@@ -96,4 +97,5 @@ static void	print_cmd(void *ptr_cmd)
 
 void	print_commands(t_list *cmds)
 {
+    ft_lstiter(cmds, print_cmd);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 16:59:09 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/04/13 14:33:46 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/04/13 14:45:16 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,6 @@ void	stack_rrotate(t_list **stack);
 
 void	run_command(t_list **stack_a, t_list **stack_b, t_list **cmds,
 			enum e_pscmd cmd);
+void	print_commands(t_list *cmds)
 
 #endif // PUSH_SWAP_H
