9674332f03f434534017c0f70f3c213fa4ebb2bf
Dominik Kaiser <dkaiser@2-E-4.42heilbronn.de>
2024-04-13 15:31:29 +0200

    Add radixsort

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ LIBFT = libft
 CC = cc
 CFLAGS = -Wall -Wextra -Werror
 
-SRC_FILES = main.c input_handling.c stack_utils.c command_handling.c
+SRC_FILES = main.c input_handling.c stack_utils.c command_handling.c sorting.c
 OBJ_FILES = $(SRC_FILES:%.c=%.o)
 
 LIB_DIR = $(LIBFT)
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 17:03:30 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/04/13 15:04:10 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/04/13 15:31:05 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,27 +20,24 @@ static void	print_content(void *content)
 int	main(int argc, char *argv[])
 {
 	t_list	*stack_a;
-	t_list *stack_b;
-	t_list *pscmds;
+	t_list	*stack_b;
+	t_list	*pscmds;
 
 	stack_a = create_stack(argc, argv);
 	if (!stack_a)
 	{
 		ft_putendl_fd("Error", 2);
-		return 1;
+		return (1);
 	}
 	stack_b = NULL;
 	pscmds = NULL;
-
 	stack_sort(&stack_a, &stack_b, &pscmds);
-
-
 	// TODO: Optimize commands
+	print_commands(pscmds);
 	ft_printf("A:");
 	ft_lstiter(stack_a, print_content);
 	ft_printf("\nB:");
 	ft_lstiter(stack_b, print_content);
-	ft_printf("\n\nCMDS:\n");
-	print_commands(pscmds);
-	return 0;
+	ft_printf("\n");
+	return (0);
 }
