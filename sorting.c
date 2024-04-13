3d5057371652145189cbf643d99f8628932e1615
Dominik Kaiser <dkaiser@2-E-4.42heilbronn.de>
2024-04-13 20:01:38 +0200

    Create function for step-by-step sorting

--- sorting.c
+++ sorting.c
@@ -6,44 +6,48 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 15:04:19 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/04/13 17:43:18 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/04/13 19:29:05 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "push_swap.h"
 
-
-
-static void stack_radixsort(t_list **stack_a, t_list **stack_b, t_list **cmds)
+static void	radixsort_step(t_list **stack_a, t_list **stack_b, t_list **cmds,
+		int bit)
 {
-    int bit;
-    int max;
-    int i;
-    int max_bits;
+	int	i;
+	int	max;
 
-    bit = 0;
-    max_bits = 9;
-
-    while (bit < max_bits)
-    {
-        i = 0;
-        max = ft_lstsize(*stack_a);
-        while (i < max)
-        {
-            if (*stack_a && (*(int *)(*stack_a)->content >> bit) % 2 == 0)
-                run_command(stack_a, stack_b, cmds, PB);
-            else if ((*stack_a)->next)
-                run_command(stack_a, stack_b, cmds, RA);
-            i++;
-        }
-        while (*stack_b)
-            run_command(stack_a, stack_b, cmds, PA);
-        bit++;
-    }
+	i = 0;
+	max = ft_lstsize(*stack_a);
+	while (i < max)
+	{
+		if (*stack_a && (*(int *)(*stack_a)->content >> bit) % 2 == 0)
+			run_command(stack_a, stack_b, cmds, PB);
+		else if ((*stack_a)->next)
+			run_command(stack_a, stack_b, cmds, RA);
+		i++;
+	}
+	while (*stack_b)
+		run_command(stack_a, stack_b, cmds, PA);
 }
 
-void stack_sort(t_list **stack_a, t_list **stack_b, t_list **cmds)
+static void	stack_radixsort(t_list **stack_a, t_list **stack_b, t_list **cmds)
 {
-    stack_radixsort(stack_a, stack_b, cmds);
+	int	bit;
+	int	max_bits;
+
+	bit = 0;
+	max_bits = 9;
+	while (bit < max_bits)
+	{
+		radixsort_step(stack_a, stack_b, cmds, bit);
+		bit++;
+	}
+}
+
+void	stack_sort(t_list **stack_a, t_list **stack_b, t_list **cmds)
+{
+	stack_radixsort(stack_a, stack_b, cmds);
 }
